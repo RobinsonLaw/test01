e13cbea49bd3fc64fedc4f66b134d13bbfd482db
RobinsonLaw <koksing1@gmail.com>
2025-06-09 22:14:22 +0800

    solved vercel and local runinng problems

--- api/app.py
+++ api/app.py
@@ -1,6 +1,8 @@
 from flask import Flask,request,make_response,render_template,send_from_directory
-from .utils.auth import encode,decode
-
+try:
+    from .utils.auth import encode,decode
+except ImportError:
+    from utils.auth import encode,decode
 
 app=Flask(__name__)
 @app.route('/')
@@ -54,5 +56,5 @@ def favicon():
 def favicon_png():
     return send_from_directory('public', 'favicon.png')
 
-# if __name__ == "__main__":
-#     app.run(debug=True)flask
+if __name__ == "__main__":
+    app.run(debug=True)
