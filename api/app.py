f3358fe1415de728f7278c9c6a7555981c54ae88
RobinsonLaw <koksing1@gmail.com>
2025-06-09 19:55:08 +0800

    add home page

--- api/app.py
+++ api/app.py
@@ -1,10 +1,10 @@
-from flask import Flask,request,make_response
+from flask import Flask,request,make_response,render_template
 from .utils.auth import encode,decode
 
 app=Flask(__name__)
 @app.route('/')
 def index():
-    return "Hello world"
+    return render_template('home.html')
 
 @app.route('/getkey')
 def getkey():
